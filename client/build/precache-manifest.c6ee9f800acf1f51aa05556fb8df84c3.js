d29f2e84479475d501dbd83f156888ff761af700
Samuel Sholib <samuelsholib12@gmail.com>
2022-03-01 18:39:30 -0800

    gitignore updated

---
+++ client/build/precache-manifest.c6ee9f800acf1f51aa05556fb8df84c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7bebb682a76cf9d5cada663e3ae2d71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a39bf76f524c92e44a0",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "fdde77eb1a26edd75cab",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "8a39bf76f524c92e44a0",
+    "url": "/static/js/2.0a76e87f.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.0a76e87f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdde77eb1a26edd75cab",
+    "url": "/static/js/main.792d992e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
